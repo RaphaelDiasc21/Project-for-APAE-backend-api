80c99dc1dd34839d89db3085204ef3a42eaa137c
RaphaelDias21 <raphaeldiasc16@gmail.com>
2020-04-05 20:21:50 -0300

    inital commit

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+    namespace App\Controllers;
+
+    use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LoginController extends Controller{
+
+                public function login(Request $request)
+                {
+                        $input = $request->only('email', 'password');
+                        $token = null;
+
+                        if (!$token = JWTAuth::attempt($input)) {
+                            return response()->json([
+                                'success' => false,
+                                'message' => 'Invalid Email or Password',
+                            ], 401);
+                         }
+                        
+                        return $token;
+                }
+        }
